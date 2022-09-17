f8f503fbd1f6633fb6e0141d5e951e19f21348c2
chenjingzhi <1286576927@qq.com>
2022-09-17 11:50:02 +0800

    first commit
    The following sections have been completed:
    
    1.singleton; 2.factory; 3.builder; 4.prototype.

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "singleton.hpp"
+#include "factory.hpp"
+#include "builder.hpp"
+#include "prototype.hpp"
+
+using namespace std;
+
+
+int main() {
+    // 单例模式测试
+//    SingletonTest::hungry_test();
+//    SingletonTest::lazy_test();
+
+    // 建造者模式测试
+//    builder_test(true);
+//    builder_test(false);
+//    builder_test_smart(true);
+//    builder_test_smart(false);
+
+    // 原型模式测试
+//    prototype_test();
+
+    return 0;
+}
