fb756fb51c2701d435fad5079f85051a3d7b40a7
chenjingzhi <1286576927@qq.com>
2022-10-06 13:48:48 +0800

    Added the facade pattern section. This section contains only descriptions, not specific examples.

---
+++ include/Structural/facade.hpp
@@ -0,0 +1,14 @@
+//
+// Created by 12865 on 2022/10/6.
+//
+
+#ifndef DESIGNPATTERNS_FACADE_HPP
+#define DESIGNPATTERNS_FACADE_HPP
+
+
+// 门面模式（外观模式）：
+// 1.本质就是，根据业务的需要，将原有的多个接口进行进一步的封装
+// 2.封装的新接口可以与原接口放在一起（比如：放在原来的类中），也可以重新抽象出一层（比如：抽象出一个新的类或模块）
+
+
+#endif //DESIGNPATTERNS_FACADE_HPP
